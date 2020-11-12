0992af252fb9d612a6a95e8c15bb1a125e6e2e20
edgar <edgarendriulis@gmail.com>
2020-11-12 22:25:19 +0000

    Node-challenge-chat-server-Amanul

--- server.js
+++ server.js
@@ -14,10 +14,22 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
+const aNewMessage = {
+  id: 1,
+  from: "Ameer",
+  text: "Hello world",
+};
+
+const aNewMessage2 = {
+  id: 2,
+  from: "Mahbub",
+  text: "The weather is attrocious",
+};
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const messages = [welcomeMessage, aNewMessage, aNewMessage2];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -27,13 +39,13 @@ app.get("/messages", function (request, response) {
   response.json(messages);
 });
 
-app.post("/messages", function (request, response) {
-  let newMessage = request.body;
-  console.log(request.body)
-  newMessage.id = messages.length;
-  messages.push(newMessage);
-  response.send("Message created")
-});
+// app.post("/messages", function (request, response) {
+//   let newMessage = request.body;
+//   console.log(request.body)
+//   newMessage.id = messages.length;
+//   messages.push(newMessage);
+//   response.send("Message created")
+// });
 
 app.get("/messages/:id", function (request, response) {
   
@@ -42,7 +54,52 @@ app.get("/messages/:id", function (request, response) {
   response.json(foundMessage);
 });
 
-const listener = app.listen(process.env.PORT || 4000, function () {
+app.delete("/messages/:id", function (request, response) {
+  let id = request.params.id;
+  messages.forEach(function(message, index) {
+    if(message.id == id) {
+      messages.splice(index, 1);
+    }
+  })
+  response.json({ success: true });
+});
+
+app.post("/messages", function(request, response) {
+  let newMessage = request.body;
+  // console.log("incoming messages.root");
+  if(newMessage.from && newMessage.text) {
+    newMessage.id = messages.length;
+    let messageArray = messages.push(newMessage)
+    response.json({ success: messageArray });
+  }
+  response.sendStatus(400);
+});
+
+app.get("/output", function(request, response) {
+  // response.send("hello")
+  let count = 0;
+  let iterationContinue = true;
+  let i = messages.length-1;
+  let recentTenMessages = [];
+  while(iterationContinue) {
+    recentTenMessages.push(messages[i]);
+    count = count + 1;
+    i = i - 1;
+    if(count >= 10 || i < 0) {
+      iterationContinue = false;
+    }
+  } 
+  response.send(recentTenMessages);
+  console.log(recentTenMessages);
+})
+
+app.get("/search", function (request, response) {
+  let givenWord = request.query.term;
+  let foundMessage = messages.filter((message)=> message.text.toLowerCase().includes(givenWord.toLowerCase()))
+  response.json(foundMessage);
+});
+
+const listener = app.listen(process.env.PORT || 8000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
